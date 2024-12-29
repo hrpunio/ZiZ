97c149d13573b49b773e045335009e456c640a00
tomasz przechlewski <rudolf.von.ems@gmail.com>
2024-12-29 16:51:21 +0100

    import

---
+++ zz.sh
@@ -0,0 +1,165 @@
+#!/bin/bash
+B="wget --no-check-certificate"
+
+## 1942 ###
+Y=1942
+
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_001/NDIGCZAS095173_${Y}_001.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_002/NDIGCZAS095173_${Y}_002.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_003/NDIGCZAS095173_${Y}_003.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_004/NDIGCZAS095173_${Y}_004.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_005/NDIGCZAS095173_${Y}_005.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_006/NDIGCZAS095173_${Y}_006.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_007/NDIGCZAS095173_${Y}_007.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_008/NDIGCZAS095173_${Y}_008.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_009/NDIGCZAS095173_${Y}_009.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_010/NDIGCZAS095173_${Y}_010.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_011/NDIGCZAS095173_${Y}_011.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_012/NDIGCZAS095173_${Y}_012.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_013/NDIGCZAS095173_${Y}_013.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_014/NDIGCZAS095173_${Y}_014.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_015/NDIGCZAS095173_${Y}_015.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_016/NDIGCZAS095173_${Y}_016.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_017/NDIGCZAS095173_${Y}_017.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_018/NDIGCZAS095173_${Y}_018.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_019/NDIGCZAS095173_${Y}_019.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_020/NDIGCZAS095173_${Y}_020.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_021/NDIGCZAS095173_${Y}_021.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_022/NDIGCZAS095173_${Y}_022.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_023/NDIGCZAS095173_${Y}_023.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_024/NDIGCZAS095173_${Y}_024.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_025/NDIGCZAS095173_${Y}_025.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_026/NDIGCZAS095173_${Y}_026.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_027/NDIGCZAS095173_${Y}_027.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_028/NDIGCZAS095173_${Y}_028.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_029/NDIGCZAS095173_${Y}_029.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_031/NDIGCZAS095173_${Y}_031.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_032/NDIGCZAS095173_${Y}_032.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_033/NDIGCZAS095173_${Y}_033.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_034/NDIGCZAS095173_${Y}_034.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_035/NDIGCZAS095173_${Y}_035.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_036/NDIGCZAS095173_${Y}_036.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_037/NDIGCZAS095173_${Y}_037.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_038/NDIGCZAS095173_${Y}_038.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_039/NDIGCZAS095173_${Y}_039.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_040/NDIGCZAS095173_${Y}_040.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_041/NDIGCZAS095173_${Y}_041.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_042/NDIGCZAS095173_${Y}_042.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_043/NDIGCZAS095173_${Y}_043.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_044/NDIGCZAS095173_${Y}_044.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_045/NDIGCZAS095173_${Y}_045.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_046/NDIGCZAS095173_${Y}_046.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_047/NDIGCZAS095173_${Y}_047.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_048/NDIGCZAS095173_${Y}_048.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_049/NDIGCZAS095173_${Y}_049.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_050/NDIGCZAS095173_${Y}_050.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_051/NDIGCZAS095173_${Y}_051.pdf
+
+## 1943 ###
+
+Y=1943
+
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_001/NDIGCZAS095173_${Y}_001.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_002/NDIGCZAS095173_${Y}_002.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_003/NDIGCZAS095173_${Y}_003.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_004/NDIGCZAS095173_${Y}_004.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_005/NDIGCZAS095173_${Y}_005.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_006/NDIGCZAS095173_${Y}_006.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_007_008/NDIGCZAS095173_${Y}_007_008.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_009/NDIGCZAS095173_${Y}_009.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_010/NDIGCZAS095173_${Y}_010.pdf
+##
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_011_012/NDIGCZAS095173_${Y}_011_012.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_013_014/NDIGCZAS095173_${Y}_013_014.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_015_016/NDIGCZAS095173_${Y}_015_016.pdf
+##
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_017/NDIGCZAS095173_${Y}_017.pdf
+##
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_018_019/NDIGCZAS095173_${Y}_018_019.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_020_021/NDIGCZAS095173_${Y}_020_021.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_022_023/NDIGCZAS095173_${Y}_022_023.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_024_025/NDIGCZAS095173_${Y}_024_025.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_026_027/NDIGCZAS095173_${Y}_026_027.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_028_029/NDIGCZAS095173_${Y}_028_029.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_030_031/NDIGCZAS095173_${Y}_030_031.pdf
+##
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_032/NDIGCZAS095173_${Y}_032.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_033/NDIGCZAS095173_${Y}_033.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_034/NDIGCZAS095173_${Y}_034.pdf
+
+exit
+
+## 1940
+Y=1940
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_001/NDIGCZAS095173_${Y}_001.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_002/NDIGCZAS095173_${Y}_002.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_003/NDIGCZAS095173_${Y}_003.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_004/NDIGCZAS095173_${Y}_004.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_005/NDIGCZAS095173_${Y}_005.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_006/NDIGCZAS095173_${Y}_006.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_007/NDIGCZAS095173_${Y}_007.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_008/NDIGCZAS095173_${Y}_008.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_009/NDIGCZAS095173_${Y}_009.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_010/NDIGCZAS095173_${Y}_010.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_011/NDIGCZAS095173_${Y}_011.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_012/NDIGCZAS095173_${Y}_012.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_013/NDIGCZAS095173_${Y}_013.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_014/NDIGCZAS095173_${Y}_014.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_015/NDIGCZAS095173_${Y}_015.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_016/NDIGCZAS095173_${Y}_016.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_017/NDIGCZAS095173_${Y}_017.pdf
+##
+## 1941
+Y=1941
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_001/NDIGCZAS095173_${Y}_001.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_002/NDIGCZAS095173_${Y}_002.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_003/NDIGCZAS095173_${Y}_003.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_004/NDIGCZAS095173_${Y}_004.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_005/NDIGCZAS095173_${Y}_005.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_006/NDIGCZAS095173_${Y}_006.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_007/NDIGCZAS095173_${Y}_007.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_008/NDIGCZAS095173_${Y}_008.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_009/NDIGCZAS095173_${Y}_009.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_010/NDIGCZAS095173_${Y}_010.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_011/NDIGCZAS095173_${Y}_011.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_012/NDIGCZAS095173_${Y}_012.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_013/NDIGCZAS095173_${Y}_013.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_014/NDIGCZAS095173_${Y}_014.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_015/NDIGCZAS095173_${Y}_015.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_016/NDIGCZAS095173_${Y}_016.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_017/NDIGCZAS095173_${Y}_017.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_018/NDIGCZAS095173_${Y}_018.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_019/NDIGCZAS095173_${Y}_019.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_020/NDIGCZAS095173_${Y}_020.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_021/NDIGCZAS095173_${Y}_021.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_022/NDIGCZAS095173_${Y}_022.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_023/NDIGCZAS095173_${Y}_023.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_024/NDIGCZAS095173_${Y}_024.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_025/NDIGCZAS095173_${Y}_025.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_026/NDIGCZAS095173_${Y}_026.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_027/NDIGCZAS095173_${Y}_027.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_028/NDIGCZAS095173_${Y}_028.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_029/NDIGCZAS095173_${Y}_029.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_030/NDIGCZAS095173_${Y}_030.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_031/NDIGCZAS095173_${Y}_031.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_032/NDIGCZAS095173_${Y}_032.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_033/NDIGCZAS095173_${Y}_033.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_034/NDIGCZAS095173_${Y}_034.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_035/NDIGCZAS095173_${Y}_035.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_036/NDIGCZAS095173_${Y}_036.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_037/NDIGCZAS095173_${Y}_037.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_038/NDIGCZAS095173_${Y}_038.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_039/NDIGCZAS095173_${Y}_039.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_040/NDIGCZAS095173_${Y}_040.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_041/NDIGCZAS095173_${Y}_041.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_042/NDIGCZAS095173_${Y}_042.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_043/NDIGCZAS095173_${Y}_043.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_044/NDIGCZAS095173_${Y}_044.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_045/NDIGCZAS095173_${Y}_045.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_046/NDIGCZAS095173_${Y}_046.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_047/NDIGCZAS095173_${Y}_047.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_048/NDIGCZAS095173_${Y}_048.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_049/NDIGCZAS095173_${Y}_049.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_050/NDIGCZAS095173_${Y}_050.pdf
+$B https://archive.org/download/jbc.bj.uj.edu.pl.NDIGCZAS095173_${Y}_051/NDIGCZAS095173_${Y}_051.pdf
+
